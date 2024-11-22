d4206c9cfd9b17aefda0f9e123a253f4e61dc0d7
Heba Ali <85513159+Heba-Sakr@users.noreply.github.com>
2024-11-22 15:39:00 +0100

    Initial commit for Angular and Node.js

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, () => console.log('Backend running on http://localhost:3000'));
